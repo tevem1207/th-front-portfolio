b528f88ab8cb7807f1b3ee042db5195d4b5ca152
tevem1207 <tevem1207@gmail.com>
2023-03-07 23:47:11 +0900

    [Feat] navbar style change

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="nav-bar">
-    <div class="nav-title font-14">
-      <p>황태희</p>
-      <p>웹 개발 포트폴리오</p>
+    <div class="nav-bar-inner">
+      <div class="nav-title font-14">
+        <p>황태희</p>
+        <p>웹 개발 포트폴리오</p>
+      </div>
+      <MenuButton />
     </div>
-    <!-- <div class="nav-burger">
-      <IconMenuBurger class="icon-24" />
-    </div> -->
-    <MenuButton />
   </div>
 </template>
 
@@ -21,16 +20,23 @@ import MenuButton from "./MenuButton.vue";
   height: 60px;
   position: fixed;
   display: flex;
-  align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   z-index: 1;
-
   border-bottom: 1px solid var(--color-border);
   background-color: var(--color-background-soft);
 }
 
+.nav-bar-inner {
+  max-width: 830px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
 .nav-title {
   width: 134px;
-  margin-left: 13px;
+  margin-left: 18px;
 }
 </style>
