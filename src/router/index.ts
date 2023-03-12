ac0c54299cb926a529f469eb858dcdc7d13df14f
tevem1207 <tevem1207@gmail.com>
2023-03-13 02:51:25 +0900

    [Feat] temporary router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,14 +3,19 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: "/",
+    //   name: "home",
+    //   component: () => import("@/views/HomeView.vue"),
+    // },
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   component: () => import("@/views/AboutView.vue"),
+    // },
     {
       path: "/",
       name: "home",
-      component: () => import("@/views/HomeView.vue"),
-    },
-    {
-      path: "/about",
-      name: "about",
       component: () => import("@/views/AboutView.vue"),
     },
   ],
